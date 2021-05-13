00c7b683245918406c5d099a0ab557ce517f99e1
Lee Seung Yong <73208493+dltmddyd321@users.noreply.github.com>
2021-05-13 16:23:09 -0700

    2021_05_13

---
+++ 2438별찍기1.py
@@ -0,0 +1,5 @@
+N = int(input())
+
+for i in range(1,N+1):
+    print(" "*(N-i)+"*"*i)
+    #반대 방향 전환을 위해 역방향 공백 추가 
---
+++ 8958_OX점수.py
@@ -0,0 +1,18 @@
+a = int(input())
+
+for i in range(a): 
+    b = input
+    c = 1 
+    sum = 0    
+    x = list(b) 
+    for i in x:
+        if i == 'O':
+            sum += c
+            c += 1
+        else:
+            c = 1
+    print(sum)
+
+#0을 만난다면 sum에 1씩 증가하는 b를 더해주고,
+#x를 만난다면 b는 계속 1이다. 
+    
